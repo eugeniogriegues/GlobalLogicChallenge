f8e85b5e88f0bc49705df913f41e2d9f714265f8
Eugenio Griegues <eugeniofiori@gmail.com>
2021-11-25 21:19:24 -0300

    Update AdapterItemList.kt
    viewBinding fixed (dataBinding)

--- app/src/main/java/com/example/globallogicchallenge/view/AdapterItemList.kt
+++ app/src/main/java/com/example/globallogicchallenge/view/AdapterItemList.kt
@@ -4,16 +4,17 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.globallogicchallenge.R
+import com.example.globallogicchallenge.databinding.ItemBinding
 import com.example.globallogicchallenge.model.Item
 
 class AdapterItemList (private val itemList: List<Item>) : RecyclerView.Adapter<AdapterItemList.MainViewHolder>() {
 
     inner class MainViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val binding = ItemBinding.bind(itemView)
 
         init {          // TODO ****  listener para cada item del recyclerView ****
 
@@ -36,17 +37,9 @@ class AdapterItemList (private val itemList: List<Item>) : RecyclerView.Adapter<
 
         fun bindData(item: Item) {
 
-            val title = itemView.findViewById<TextView>(R.id.output_title)
-            val description = itemView.findViewById<TextView>(R.id.output_description)
-            val image = itemView.findViewById<ImageView>(R.id.output_image)
-
-
-
-
-
-            title.text = item.title
-            description.text = item.description
-            image.load(item.image.replace("photos.ar","photos"))
+            binding.outputTitle.text = item.title
+            binding.outputDescription.text = item.description
+            binding.outputImage.load(item.image.replace("photos.ar","photos"))
 
 
         }
